a2876089f1c44d0299007d6a3bb8a8915da30d73
vivien-laasch <vivien.laasch@outlook.com>
2022-09-20 11:36:45 +0200

    aaah

--- resources/create_db.sql
+++ resources/create_db.sql
@@ -1,14 +1,11 @@
-drop database if exists 'teamtwo';
+drop database if exists `teamtwo`;
 
-create database if not exists 'teamtwo';
+create database if not exists `teamtwo`;
 
-use 'teamtwo';
+use `teamtwo`;
 
-create table if not exists 'measurements' (
+create table if not exists `measurements` (
     id INT primary key auto_increment not null,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     humidity FLOAT(10,2) unsigned zerofill,
 	heat_index FLOAT(10,2) unsigned zerofill);
-   
-select  from measurements;
-   
