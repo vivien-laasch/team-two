d13ad00f4698fe97e869f041ae19d2d00566ecf0
vivien-laasch <vivien.laasch@outlook.com>
2022-09-20 11:38:56 +0200

    formatting

--- resources/create_db.sql
+++ resources/create_db.sql
@@ -8,4 +8,4 @@ create table if not exists `measurements` (
     id INT primary key auto_increment not null,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     humidity FLOAT(10,2) unsigned zerofill,
-	heat_index FLOAT(10,2) unsigned zerofill);
+    heat_index FLOAT(10,2) unsigned zerofill);
