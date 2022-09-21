d971fd4a5e9f451698cea4edbc1e9e16ed79cd7b
vivien-laasch <vivien.laasch@outlook.com>
2022-09-21 11:43:05 +0200

    lol

---
+++ resources/create_dummy_data.sql
@@ -0,0 +1,12 @@
+drop database if exists `teamtwo`;
+
+create database if not exists `teamtwo`;
+
+use `teamtwo`;
+
+create table if not exists `measurements` (
+    id INT primary key auto_increment not null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    humidity FLOAT(10,2) unsigned zerofill,
+    temperature FLOAT(10,2) unsigned zerofill,
+    heat_index FLOAT(10,2) unsigned zerofill);
