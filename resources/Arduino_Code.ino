7621e7b02ee7bfb3fa17603dfc52a46b81f47538
vivien-laasch <vivien.laasch@outlook.com>
2022-09-22 12:03:33 +0200

    added co2

--- resources/Arduino_Code.ino
+++ resources/Arduino_Code.ino
@@ -1,56 +1,68 @@
 #include "DHT.h"
+#include <LiquidCrystal.h>
 
-#define pin01 A0
-#define pin02 A1
-#define pinSensor A2
+#define pinMotor1 A2
+#define pinMotor2 A3
+#define pinTemp A1
+#define pinCo2 A0
 #define DHTTYPE DHT11 
 
-DHT dht(pinSensor, DHTTYPE);
+DHT dht(pinTemp, DHTTYPE);
 int state = 0;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  lcd.begin(16, 2);
 }
 
 void loop() {
   delay(5000);
   float h = dht.readHumidity();
   float t = dht.readTemperature();
+  int co2 = analogRead(pinCo2);
 
-  if (isnan(h) || isnan(t) ) {
+  if (isnan(h) || isnan(t) || isnan(co2) ) {
     return;
   }
+  
   float hic = dht.computeHeatIndex(t, h, false);
 
 
   if(hic > 25 && state == 0){
+    Serial.println("state is 0");
     for(int i = 0; i<255; i++){
-      analogWrite(pin01, i);
-      analogWrite(pin02, 0);
+      analogWrite(pinMotor1, i);
+      analogWrite(pinMotor2, 0);
       delay(5);
     }    
     state = 1;
     
   }else if(hic < 25 && state == 1){
+     Serial.println("state is 1");
      for(int i = 0; i<255; i++){
-      analogWrite(pin01, 0);
-      analogWrite(pin02, i);
+      analogWrite(pinMotor1, 0);
+      analogWrite(pinMotor2, i);
       delay(5);
      }
      state = 0;
      
   }else{
-    analogWrite(pin01, 0);
-    analogWrite(pin02, 0);
+    analogWrite(pinMotor1, 0);
+    analogWrite(pinMotor2, 0);
   }
 
-Serial.println(hic);
 
   String data = "{\"Humidity\": ";
   data.concat(h);
   data.concat(", \"Temperature\": ");
   data.concat(t);
+  data.concat(", \"HeatIndex\": ");
+  data.concat(hic);
+  data.concat(", \"Co2\": ");
+  data.concat(co2);
   data.concat("}");
 
   Serial.println(data);
