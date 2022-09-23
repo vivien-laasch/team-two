7e348ae70459617a4e441dbc3a655445dffd34d1
vivien-laasch <vivien.laasch@outlook.com>
2022-09-23 12:21:19 +0200

    complete logic

--- resources/Arduino_Code.ino
+++ resources/Arduino_Code.ino
@@ -1,16 +1,16 @@
 #include "DHT.h"
 #include <LiquidCrystal.h>
 
+#define pinCo2 A0
+#define pinTemp A1
 #define pinMotor1 A2
 #define pinMotor2 A3
-#define pinTemp A1
-#define pinCo2 A0
 #define DHTTYPE DHT11
 #define targetTempMin 19.0
-#define targetHumMax 50
+#define targetHumMax 50.0
 #define maxCo2Ppm 1000
-#define openWindowTimer 480000
-#define closeWindowTimer 2400000
+#define openWindowTimer 420 //8 minutes
+#define closedWindowTimer 2400 //40 minutes
 
 DHT dht(pinTemp, DHTTYPE);
 bool isOpen = false;
@@ -26,7 +26,11 @@ void setup() {
 
 void loop() {
   delay(5000);
-  timer += 5000;
+  
+  if (timer <= closedWindowTimer){
+    timer += 5; //add in minutes to avoid overflow
+  }
+
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   int co2 = analogRead(pinCo2) + 700; //fake value because sensor data is inaccurate
@@ -38,40 +42,35 @@ void loop() {
   float hic = dht.computeHeatIndex(t, h, false);
   printData(t, h, hic, co2);
 
-  if(isOpen && timer >= openWindowTimer){
-    if(doCloseWindow(t,h,co2)){
-      controlMotor(false);
-      isOpen = false;
-      timer = 0;
-    }
-  }
-
-  if(isOpen && doCloseWindow(t,h,co2) && timer >= openWindowTimer){
-    controlMotor(false);
-    isOpen = false;
-    timer = 0;
-  }
-  if(!isOpen && doOpenWindow(t,co2) && timer >= openWindowTimer){
+  if(!isOpen && timer >= closedWindowTimer && doOpenWindow(h,co2)){
     controlMotor(true);
     isOpen = true;
+    timer = 0;
+  }
+
+  if(isOpen && timer >= openWindowTimer && doCloseWindow(t,h,co2)){
+    controlMotor(false);
+    isOpen = false;
     timer = 0;
   }
 }
 
 void controlMotor(bool doOpen){
   if(doOpen){
-    for(int i = 0; i<255; i++){
-      analogWrite(pinMotor1, i);
+    for(int i = 0; i<1000; i++){
+      analogWrite(pinMotor1, 150);
       analogWrite(pinMotor2, 0);
       delay(5);
     }        
   }else if(!doOpen){
-     for(int i = 0; i<255; i++){
+     for(int i = 0; i<1000; i++){
       analogWrite(pinMotor1, 0);
-      analogWrite(pinMotor2, i);
+      analogWrite(pinMotor2, 150);
       delay(5);
      }
   }
+  analogWrite(pinMotor1, 0);
+  analogWrite(pinMotor2, 0);
 }
 
 bool doOpenWindow(float h, int co2){
